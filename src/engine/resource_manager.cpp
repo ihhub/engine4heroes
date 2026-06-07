453ca3f6cc22be41b3379f3de8547662fd3b4363
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2026-06-07 23:34:31 +0800

    Remove pragma once

--- src/engine/resource_manager.cpp
+++ src/engine/resource_manager.cpp
@@ -18,8 +18,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#pragma once
-
 #include "resource_manager.h"
 
 #include <map>
