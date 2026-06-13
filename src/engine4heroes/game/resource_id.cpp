64786987c4d0c5cb81e620d3a3cd02edcb1cacbd
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2026-06-13 23:00:11 +0800

    Fix code style

--- src/engine4heroes/game/resource_id.cpp
+++ src/engine4heroes/game/resource_id.cpp
@@ -30,31 +30,29 @@ namespace
         const char * string{ nullptr };
     };
 
-    const std::array<ResourceInfo, ImageId::COUNT> imageNames{ {
-        { ImageId::NONE, ""},
-        { ImageId::LAYERS_MENU_MAIN_0800, "layers.menu.main.0800.h4d" },
-        { ImageId::LAYERS_MENU_MAIN_1024, "layers.menu.main.1024.h4d" },
-        { ImageId::LAYERS_MENU_MAIN_1280, "layers.menu.main.1280.h4d" },
-        { ImageId::BITMAP_RAW_MENU_MAIN_0800, "bitmap_raw.menu.main.0800.h4d" },
-        { ImageId::BITMAP_RAW_MENU_MAIN_1024, "bitmap_raw.menu.main.1024.h4d" },
-        { ImageId::BITMAP_RAW_MENU_MAIN_1280, "bitmap_raw.menu.main.1280.h4d" },
-        { ImageId::ANIMATION_SAMPLE_CURSOR, "animation.sample_cursor.h4d" },
-        { ImageId::FONT_PROSE_ANTIQUE_10, "font.prose_antique.10.h4d" },
-        { ImageId::FONT_PROSE_ANTIQUE_12, "font.prose_antique.12.h4d" },
-        { ImageId::FONT_PROSE_ANTIQUE_14, "font.prose_antique.14.h4d" },
-        { ImageId::FONT_PROSE_ANTIQUE_16, "font.prose_antique.16.h4d" },
-        { ImageId::FONT_PROSE_ANTIQUE_18, "font.prose_antique.18.h4d" },
-        { ImageId::FONT_PROSE_ANTIQUE_20, "font.prose_antique.20.h4d" },
-        { ImageId::FONT_PROSE_ANTIQUE_22, "font.prose_antique.22.h4d" },
-        { ImageId::FONT_PROSE_ANTIQUE_24, "font.prose_antique.24.h4d" },
-        { ImageId::FONT_PROSE_ANTIQUE_26, "font.prose_antique.26.h4d" }
-        } };
+    const std::array<ResourceInfo, ImageId::COUNT> imageNames{ { { ImageId::NONE, "" },
+                                                                 { ImageId::LAYERS_MENU_MAIN_0800, "layers.menu.main.0800.h4d" },
+                                                                 { ImageId::LAYERS_MENU_MAIN_1024, "layers.menu.main.1024.h4d" },
+                                                                 { ImageId::LAYERS_MENU_MAIN_1280, "layers.menu.main.1280.h4d" },
+                                                                 { ImageId::BITMAP_RAW_MENU_MAIN_0800, "bitmap_raw.menu.main.0800.h4d" },
+                                                                 { ImageId::BITMAP_RAW_MENU_MAIN_1024, "bitmap_raw.menu.main.1024.h4d" },
+                                                                 { ImageId::BITMAP_RAW_MENU_MAIN_1280, "bitmap_raw.menu.main.1280.h4d" },
+                                                                 { ImageId::ANIMATION_SAMPLE_CURSOR, "animation.sample_cursor.h4d" },
+                                                                 { ImageId::FONT_PROSE_ANTIQUE_10, "font.prose_antique.10.h4d" },
+                                                                 { ImageId::FONT_PROSE_ANTIQUE_12, "font.prose_antique.12.h4d" },
+                                                                 { ImageId::FONT_PROSE_ANTIQUE_14, "font.prose_antique.14.h4d" },
+                                                                 { ImageId::FONT_PROSE_ANTIQUE_16, "font.prose_antique.16.h4d" },
+                                                                 { ImageId::FONT_PROSE_ANTIQUE_18, "font.prose_antique.18.h4d" },
+                                                                 { ImageId::FONT_PROSE_ANTIQUE_20, "font.prose_antique.20.h4d" },
+                                                                 { ImageId::FONT_PROSE_ANTIQUE_22, "font.prose_antique.22.h4d" },
+                                                                 { ImageId::FONT_PROSE_ANTIQUE_24, "font.prose_antique.24.h4d" },
+                                                                 { ImageId::FONT_PROSE_ANTIQUE_26, "font.prose_antique.26.h4d" } } };
 
     const std::array<ResourceInfo, AudioId::COUNT> audioNames{ {
-        { AudioId::NONE, ""},
+        { AudioId::NONE, "" },
         { AudioId::MAIN_MENU, "sound.main_menu.h4d" },
         { AudioId::MISCELLANEOUS_BUTTON, "sound.miscellaneous.button.h4d" },
-        } };
+    } };
 }
 
 namespace GameResource
